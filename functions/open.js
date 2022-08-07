a59c84ec8333ed505499913fe561daeb974b308e
m-avagyan <martikavagyan1@gmail.com>
2022-08-07 16:21:36 +0400

    feat: open func

---
+++ functions/open.js
@@ -0,0 +1,65 @@
+const getFileType = require('./fileType');
+
+const defaultConfigs = {
+  newTab: true,
+  width: '100%',
+  height: '100%',
+};
+
+/**
+ *
+ * @param { Boolean } newTab
+ * @returns
+ */
+
+const createTab = (newTab) => {
+  if (newTab) {
+    return window.open('about:blank');
+  } else {
+    return window.location.replace('about:blank');
+  }
+};
+
+/**
+ *
+ * @param { String } blob
+ * @param { { newTab: Boolean, width: String, height: String } } configs
+ */
+
+const open = (blob, configs = defaultConfigs) => {
+  const fileType = getFileType(blob);
+
+  const { newTab, width, height } = configs;
+
+  if (fileType === 'pdf') {
+    const tab = createTab(newTab);
+
+    setTimeout(() => {
+      const body = tab.document.body;
+      const iframe = document.createElement('iframe');
+
+      body.appendChild(iframe);
+
+      iframe.src = src;
+
+      iframe.style.width = width;
+      iframe.style.height = height;
+    }, 0);
+  } else {
+    const tab = createTab(newTab);
+
+    setTimeout(() => {
+      const body = tab.document.body;
+      const image = tab.document.createElement('img');
+
+      body.appendChild(image);
+      image.src = src;
+
+      image.style.width = width;
+      image.style.display = 'block';
+      image.style.margin = '0 auto';
+    }, 0);
+  }
+};
+
+module.exports = open;
