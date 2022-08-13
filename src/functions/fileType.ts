e07e984bc003a8d15e88b5e81e0a2d11ba9e8068
m-avagyan <martikavagyan1@gmail.com>
2022-08-13 15:01:45 +0400

    fix: functions

--- src/functions/fileType.ts
+++ src/functions/fileType.ts
@@ -3,7 +3,7 @@ const getFileType = (blob: string): string | Error => {
     throw new Error('The parameter must be a blob string.');
   }
 
-  return blob.split('/')[1].split(';')[0];
+  return blob.split(':')[1].split(';')[0];
 };
 
 export default getFileType;
